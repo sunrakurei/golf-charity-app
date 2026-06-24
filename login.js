2bd88cb2073f3888a9adac70a2880e3fff01091c
sunrakurei <anutkarsh5@gmail.com>
2026-06-24 12:55:06 +0530

    Update login.js

--- login.js
+++ login.js
@@ -44,7 +44,7 @@ btn.disabled = true;
 
 try {
 
-const response = await fetch("http://localhost:5000/api/auth/login", {
+const response = await fetch("/api/auth/login", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
